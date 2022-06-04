4e64be993f5f2130556e11782955d1ed013b39d0
Anne Elvira <92056286+Annelv07@users.noreply.github.com>
2022-06-04 14:36:55 +0800

    Add files via upload

---
+++ TestNet ALGO TopUp.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 11:45:50 2022
+
+@author: User
+"""
+
+from algosdk.v2client import algod
+
+# Connecting to the Algod Client on the Algorand sandbox
+algod_address = "http://localhost:4001"
+algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+algod_client = algod.AlgodClient(algod_token, algod_address)
+
+# Declare your Address
+my_address = "Your_Address_Here"
+
+account_info = algod_client.account_info(my_address)
+print("Account balance: {} microAlgos".format(account_info.get('amount')))
